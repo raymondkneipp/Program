27d1da62bc850903d9b7658f96507f1852b69e90
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-04-16 20:41:55 +0500

    Added routine and decreased volume

--- program.py
+++ program.py
@@ -14,10 +14,11 @@ class Cardio:
 		
 		print(f'{name}{intensity}{total}')
 
-c1 = Cardio("Bike", 15, "Moderate")
-c2 = Cardio("Bike", 15, "Moderate")
-c3 = Cardio("Elliptical", 30, "Low")
-c4 = Cardio("Bike", 15, "HIIT")
+crun = Cardio("Run", 15, "Moderate")
+cbike = Cardio("Bike", 15, "Moderate")
+cwalk = Cardio("Walk", 25, "Low")
+chiit = Cardio("Bike", 15, "HIIT")
+crest = Cardio("Rest", 0, "Rest")
 
 class Stretch:
 	def __init__(self, group, name, hold, unilateral=False, rest=5):
@@ -73,35 +74,29 @@ class StretchingRoutine:
 s1 = StretchingRoutine("Full Body Stretch A", [
 	Stretch("calf", "Gastrocnemius stretch", 30, True),
 	Stretch("calf", "Shin stretch", 30),
-	Stretch("triceps", "OH triceps", 30, True),
 	Stretch("biceps", "Standing biceps stretch", 30),
-	Stretch("forearms", "Wrist extension", 30),
 	Stretch("shoulders", "Cross body shoulder stretch", 30, True),
 	Stretch("back", "Cobra stretch", 30),
 	Stretch("chest", "Wall chest stretch", 30, True),
 	Stretch("hips", "Pigeon stretch", 30, True),
 	Stretch("quads", "Standing quad stretch", 30, True),
-	Stretch("hips", "Standing hip stretch", 30, True),
 	Stretch("hamstring", "Strap assisted lying ham", 30, True),
 ], 2)
 
 s2 = StretchingRoutine("Full Body Stretch B", [
 	Stretch("calf", "Soleus stretch", 30, True),
-	Stretch("calf", "Foot stretch (toe)", 30, True),
 	Stretch("triceps", "OH triceps", 30, True),
 	Stretch("biceps", "Standing biceps stretch", 30),
 	Stretch("hamstring", "Standing hamstring", 30),
-	Stretch("forearms", "Wrist flexion", 30),
 	Stretch("shoulders", "Cross body shoulder stretch", 30, True),
 	Stretch("back", "Child's pose", 30),
-	Stretch("abs", "Bridge", 30),
 	Stretch("glutes", "Supine figure 4 glute stretch", 30, True),
 	Stretch("quads", "Standing quad stretch", 30, True),
 	Stretch("hips", "Standing hip stretch", 30, True),
 ], 2)
 
 class Exercise:
-	def __init__(self, group, name, sets, reps, rest=60, intensity=7.5, tempo=[3, 1, 2]):
+	def __init__(self, group, name, sets, reps, rest=120, intensity=7.5, tempo=[3, 1, 2]):
 		self.group = group
 		self.name = name
 		self.sets = sets
@@ -215,65 +210,45 @@ class Routine:
 
 			print(f'Total Time: {time.strftime("%H:%M:%S", time.gmtime(totalTime))}'.rjust(100))
 
-r = Routine("Push/Pull/Legs", [
-	Workout("Push A", [
-		Exercise("chest", "Smith machine bench press", 4, 8),
-		Exercise("chest", "Incline dumbbell bench press", 3, 10),
-		Exercise("chest", "Chest fly", 3, 10),
-		Exercise("shoulders", "Dumbbell shoulder press", 3, 8),
-		Exercise("shoulders", "Leaning away lateral cable raise", 3, 10),
-		Exercise("shoulders", "Rear delt machine fly", 3, 10),
-		Exercise("triceps", "Skullcrusher", 4, 10),
-		Exercise("triceps", "Cable OH triceps extention w/ rope", 3, 10),
-	]),
-	Workout("Pull A", [
-		Exercise("back", "Lat pull down", 4, 8),
-		Exercise("back", "Seated cable row", 3, 8),
-		Exercise("back", "Shrugs", 3, 10),
-		Exercise("biceps", "Close grip chin up", 4, 10),
-		Exercise("biceps", "Hammer curl", 3, 10),
-		Exercise("abs", "Bicycle crunch", 4, 10),
-		Exercise("abs", "Ab wheel", 4, 10),
-	]),
-	Workout("Legs A", [
-		Exercise("legs", "Smith machine squat", 4, 8),
-		Exercise("legs", "Lunges", 3, 10),
-		Exercise("legs", "Leg curl", 3, 10),
-		Exercise("calves", "Calf raise", 4, 10),
-		Exercise("calves", "Bent leg calf raise", 4, 10),
-		Exercise("calves", "Anterior tibialis curl", 4, 10),
-	]),
-	Workout("Push B", [
-		Exercise("chest", "Incline smith machine bench press", 4, 8),
-		Exercise("chest", "Machine chest press", 3, 10),
-		Exercise("chest", "Chest fly", 3, 10),
-		Exercise("shoulders", "Dumbbell lateral raise", 4, 10),
-		Exercise("shoulders", "Rear delt machine fly", 3, 10),
-		Exercise("triceps", "Close grip bench press", 4, 8),
-		Exercise("triceps", "Rope pushdown", 3, 10),
-	]),
-	Workout("Pull B", [
-		Exercise("back", "Pull up", 4, 8),
-		Exercise("back", "Machine row", 3, 10),
-		Exercise("back", "Shrugs", 3, 10),
-		Exercise("biceps", "EZ bar curl", 4, 10),
-		Exercise("biceps", "Machine curl", 3, 10),
-		Exercise("abs", "Decline sit up", 4, 10),
-		Exercise("abs", "Plank (1 min)", 4, 10),
-	]),
-	Workout("Legs B", [
-		Exercise("legs", "Smith machine deadlift", 4, 8),
-		Exercise("legs", "Lunges", 3, 10),
-		Exercise("legs", "Leg extension", 3, 10),
-		Exercise("calves", "Calf raise", 4, 10),
-		Exercise("calves", "Bent leg calf raise", 4, 10),
-		Exercise("calves", "Anterior tibialis curl", 4, 10),
-	]),
-	Workout("Rest", [])
+push = Workout("Push", [
+		Exercise("chest", "bench press", 3, 12),
+		Exercise("abs", "decline situp", 3, 12),
+		Exercise("chest", "incline dumbbell press", 3, 12),
+		Exercise("triceps", "triceps rope pushdown", 3, 12),
+		Exercise("chest", "chest fly", 2, 12),
+		Exercise("triceps", "triceps cable extension", 2, 12),
+		Exercise("abs", "machine crunch", 2, 12),
+	])
+
+pull = Workout("Pull", [
+		Exercise("back", "pull up", 4, 12),
+		Exercise("shoulders", "rear delt fly", 2, 12),
+		Exercise("back", "bent over row", 3, 12),
+		Exercise("biceps", "incline curl", 3, 12),
+		Exercise("back", "lat pulldown", 2, 12),
+		Exercise("biceps", "cable curl", 2, 12),
+		Exercise("shoulders", "reverse fly single arm cable", 2, 12),
+	])
+	
+legs = Workout("Legs", [
+		Exercise("legs", "squat", 3, 12),
+		Exercise("calves", "Calf press", 3, 12),
+		Exercise("legs", "leg press", 3, 12),
+		Exercise("calves", "seated calf raise", 2, 10),
+		Exercise("shoulders", "dumbbell shoulder press", 3, 12),
+		Exercise("legs", "leg curl", 2, 12),
+		Exercise("shoulders", "lateral dumbbell raise", 2, 12),
+	])
+
+rest = Workout("Rest", [])
+
+
+r = Routine("Push/Pull/Shoulders/Legs", [
+	push, pull, legs, rest, push, pull, legs
 ], [
 	s1, s2, s1, s2, s1, s2, s1
 ], [
-	c1, c2, c3, c1, c2, c3, c4
+	crun, cbike, cwalk, crest, chiit, crun, cwalk
 ])
 
 def helpMenu():
---
+++ routine.md
@@ -0,0 +1,23 @@
+# Routine
+
+| Time | Task                |
+| ---- | ------------------- |
+| 0600 | wake up             |
+|      | make bed            |
+|      | cold shower         |
+|      | 2 duolingo lessions |
+|      | black coffee        |
+|      | brush, floss, rinse |
+| 0700 | begin work          |
+| 1200 | eat                 |
+|      | work                |
+| 1330 | go to gym           |
+|      | lift                |
+|      | cardio              |
+|      | head home           |
+|      | stretch             |
+| 1600 | eat                 |
+|      | work                |
+| 2100 | shower              |
+| 2130 | read                |
+| 2200 | go to bed           |
